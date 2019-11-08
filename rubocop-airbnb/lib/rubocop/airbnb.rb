3e9a88483cf0835d2f639973b44d230eeaabcc0d
zachary_sabin <zach.sabin@airbnb.com>
2019-11-08 08:16:43 -0800

    remove debug puts

--- rubocop-airbnb/lib/rubocop/airbnb.rb
+++ rubocop-airbnb/lib/rubocop/airbnb.rb
@@ -10,7 +10,6 @@ module RuboCop
       Pathname.new(__FILE__).parent.parent.parent.expand_path.freeze
     rubocop_version = Gem.loaded_specs['rubocop'].version.to_s[0...4]
     CONFIG_DEFAULT = PROJECT_ROOT.join('config', rubocop_version, 'default.yml').freeze
-    puts CONFIG_DEFAULT
     CONFIG = Psych.safe_load(CONFIG_DEFAULT.read).freeze
 
     private_constant(*constants(false))
